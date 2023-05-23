8137218c741b777c2aebe787715bd3810763a62c
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-05-23 10:05:37 +0000

    Handle array copies with ACPY in parallel sections

--- lib/Pointer/Parallel/OpenACCSingleColumn.pm
+++ lib/Pointer/Parallel/OpenACCSingleColumn.pm
@@ -106,6 +106,36 @@ EOF
       $argspec->appendChild (&n ('<arg><arg-N><k>YDSTACK</k></arg-N>=<named-E><N><n>YLSTACK</n></N></named-E></arg>'));
     }
 
+  if (1)
+    {
+      my @acpy = &F ('.//a-stmt'
+                   . '[E-1/named-E/R-LT/array-R/section-subscript-LT/section-subscript[string(lower-bound)="JLON"]]' 
+                   . '[E-2/named-E/R-LT/array-R/section-subscript-LT/section-subscript[string(lower-bound)="JLON"]]'
+                   , $do_jlon);
+     
+      for my $acpy (@acpy)
+        {
+          my ($E1) = &F ('./E-1/named-E', $acpy);
+          my ($E2) = &F ('./E-2/named-E', $acpy);
+
+          my @lb1 = &F ('./R-LT/array-R/section-subscript-LT/section-subscript', $E1); 
+          my @lb2 = &F ('./R-LT/array-R/section-subscript-LT/section-subscript', $E2); 
+
+          my @dd1 = map { &F ('./text()[contains(string(.),":")]', $_) } @lb1;
+          my @dd2 = map { &F ('./text()[contains(string(.),":")]', $_) } @lb2;
+
+          die if (@dd1 && (! @dd2));
+          die if (@dd2 && (! @dd1));
+          next unless (@dd1 && @dd2);
+
+          $lb1[0]->replaceNode (&n ('<section-subscript>:</section-subscript>'));
+          $lb2[0]->replaceNode (&n ('<section-subscript>:</section-subscript>'));
+
+          $acpy->replaceNode (&s ('CALL ACPY (JLON, ' . $E1->textContent . ', ' . $E2->textContent . ')'));
+        }
+    }
+
+
   my @NPROMA = sort grep { $t->{$_}{nproma} } &F ('.//named-E/N', $do_jlon, 1);
 
   my ($do_jblk) = &F ('./do-construct/do-stmt[string(do-V)="JBLK"]', $par);
