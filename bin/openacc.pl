0ea3c2a2dfc161c4579d1ee67abff526d4ab60a3
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-01-31 19:28:14 +0000

    Use MODE_... -> Use MODE_..._OPENACC + change PRINT_MSG

--- bin/openacc.pl
+++ bin/openacc.pl
@@ -81,40 +81,61 @@ sub updateFile
         $fh->print ($code); 
         $fh->close ();
       }
-  }
+}
+
+sub useABOR1_ACC
+{
+  my $d = shift;
+
+  my @abor1 = &F ('.//call-stmt/procedure-designator/named-E/N/n/text()[string(.)="ABOR1"]', $d);
+
+  for my $abor1 (@abor1)
+    {
+      $abor1->setData ('ABOR1_ACC');
+    }
+  my @include = &F ('.//include[string(filename)="abor1.intfb.h"]', $d);
+  for (@include)
+    {
+      $_->unbindNode ();
+    }
+}
+
+sub changeWRITEintoPRINT
+{
+  my $d = shift;
+
+  my @write = &F ('.//write-stmt[string(./io-control-spec/io-control)="NULERR" or string(./io-control-spec/io-control)="NULOUT"]', $d);
   
-  sub useABOR1_ACC
-  {
-    my $d = shift;
+  for my $write (@write)
+    {
+      my ($output) = &F ('./output-item-LT', $write, 1);
+      $write->replaceNode (&s ("PRINT *, $output"));
+    }
+
+}
+
+sub changePRINT_MSGintoPRINT
+{
+  my $d = shift;
+
+  my @print_msg = &F ('.//call-stmt[string(procedure-designator)="PRINT_MSG"]', $d);
   
-    my @abor1 = &F ('.//call-stmt/procedure-designator/named-E/N/n/text()[string(.)="ABOR1"]', $d);
-  
-    for my $abor1 (@abor1)
-      {
-        $abor1->setData ('ABOR1_ACC');
-      }
-    my @include = &F ('.//include[string(filename)="abor1.intfb.h"]', $d);
-    for (@include)
-      {
-        $_->unbindNode ();
-      }
-  }
-  
-  sub changeWRITEintoPRINT
-  {
-    my $d = shift;
-  
-    my @write = &F ('.//write-stmt[string(./io-control-spec/io-control)="NULERR" or string(./io-control-spec/io-control)="NULOUT"]', $d);
-    
-    for my $write (@write)
-      {
-        my ($output) = &F ('./output-item-LT', $write, 1);
-        $write->replaceNode (&s ("PRINT *, $output"));
-      }
-  
-  }
-  
-  
+  for my $print_msg (@print_msg)
+    {
+      my @arg = &F ('./arg-spec/arg/ANY-E', $print_msg);
+
+      if ($arg[0]->textContent eq 'NVERB_FATAL')
+        {
+          $print_msg->replaceNode (&s ("CALL ABOR1_ACC (" . $arg[-1]->textContent . ")"));
+        }
+      else
+        {
+          $print_msg->replaceNode (&s ("PRINT *, " . $arg[-1]->textContent . ")"));
+        }
+    }
+
+}
+
 sub processSingleRoutine
 {
   my ($d, $find, %opts) = @_;
@@ -180,8 +201,6 @@ sub processSingleRoutine
       ($KIDIA, $KFDIA) = ('KST', 'KEND')
     }
   
-  
-  
   my @pointer;
   
   @pointer = &Pointer::setPointersDimensions ($d)
@@ -221,6 +240,7 @@ sub processSingleRoutine
   
   &useABOR1_ACC ($d);
   &changeWRITEintoPRINT ($d);
+  &changePRINT_MSGintoPRINT ($d);
 
 }
 
