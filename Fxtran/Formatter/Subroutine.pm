f2adea4919031540ef847d2b0cfac624d29231a1
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-06-10 19:45:25 +0200

    Process subroutine statement

---
+++ Fxtran/Formatter/Subroutine.pm
@@ -0,0 +1,41 @@
+package Fxtran::Formatter::Subroutine;
+
+use base qw (Fxtran::Formatter::block);
+
+use strict;
+
+use fxtran;
+use fxtran::parser;
+use fxtran::xpath;
+
+sub expand
+{
+  my $class = shift;
+  my ($stmt, $indent) = @_;
+
+  $stmt = $class->canonic ($stmt);
+
+  my @arg = &F ('./dummy-arg-LT/arg-N', $stmt, 1);
+
+  my ($name) = &F ('./subroutine-N', $stmt, 1);
+
+  $stmt = "SUBROUTINE $name ( &\n$indent  " .  join ("\n$indent, ", map { "$_ & " } @arg) . "\n$indent)";
+
+  $stmt = $class->reparse ($stmt);
+
+  return $stmt;
+}
+
+sub repack
+{
+  my $class = shift;
+  my ($stmt, $indent) = @_;
+
+  my @arg = &F ('./dummy-arg-LT/arg-N', $stmt, 1);
+
+  my ($name) = &F ('./subroutine-N', $stmt, 1);
+
+  $class->repackCallLikeStatement ("SUBROUTINE $name (", @arg, ")", $indent);
+}
+
+1;
