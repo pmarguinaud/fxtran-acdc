528405de7f38e8b1e42e76981f6179e325d93105
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-07-25 10:28:53 +0000

    Add FXTRAN_ACDC_GEMM modules. Append -DFXTRAN_ACDC macro when compiling with fxtran-f90. Add boot target to all objects and executables in makemaker.

---
+++ src/fxtran_acdc_gemm_mod_singleblock.F90
@@ -0,0 +1,80 @@
+MODULE FXTRAN_ACDC_GEMM_MOD_SINGLEBLOCK
+
+IMPLICIT NONE
+
+PRIVATE
+
+PUBLIC :: FXTRAN_ACDC_GEMM_SINGLEBLOCK
+
+CONTAINS
+
+SUBROUTINE FXTRAN_ACDC_GEMM_SINGLEBLOCK (KIDIA, KFDIA, TRANSA, TRANSB, M, N, K, ALPHA, A, &
+                                       & LDA, B, LDB, BETA, C, LDC, LDDONE, LDACC)
+                                ! VERINT
+INTEGER     :: KIDIA
+INTEGER     :: KFDIA
+CHARACTER*1 :: TRANSA           ! 'N'
+CHARACTER*1 :: TRANSB           ! 'T'
+INTEGER     :: M                ! KPROMA
+INTEGER     :: N                ! KLEVOUT-1   
+INTEGER     :: K                ! KLEVIN
+REAL*8      :: ALPHA            ! 1.0_JPRD
+REAL*8      :: A (LDA, *)       ! ZIN
+INTEGER     :: LDA              ! KPROMA
+REAL*8      :: B (LDB, *)       ! PINTE
+INTEGER     :: LDB              ! KLEVOUT
+REAL*8      :: BETA             ! 0.0_JPRB
+REAL*8      :: C (LDC, *)       ! ZOUT
+INTEGER     :: LDC              ! KPROMA
+LOGICAL     :: LDDONE
+LOGICAL     :: LDACC
+
+INTEGER :: JM, JN, JK
+CHARACTER*1, SAVE :: CLENV = ''
+LOGICAL :: LLSIMPLE_DGEMM = .FALSE.
+
+IF (CLENV == '') THEN
+  CALL GETENV ('LLSIMPLE_DGEMM', CLENV)
+  LLSIMPLE_DGEMM = CLENV (1:1) == '1' 
+  CLENV = '0'
+ENDIF
+
+IF (TRANSA /= 'N') STOP 1
+IF (TRANSB /= 'T') STOP 1
+
+IF (LDACC) THEN
+
+!IF (LLSIMPLE_DGEMM) THEN
+
+  !$ACC PARALLEL LOOP GANG VECTOR &
+  !$ACC&PRESENT (A, B, C) &
+  !$ACC&PRIVATE (JK, JN, JM) 
+
+  DO JM = KIDIA, KFDIA
+    DO JN = 1, N
+      C (JM, JN) = 0.
+    ENDDO
+    DO JK = 1, K
+      DO JN = 1, N
+        C (JM, JN) = C (JM, JN) + B (JN, JK) * A (JM, JK)
+      ENDDO
+    ENDDO
+  ENDDO
+
+!ELSE
+
+! CALL CUBLAS (...)
+
+!ENDIF
+
+ELSE
+
+  CALL DGEMM ('N', 'T', M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
+
+ENDIF
+
+LDDONE = .TRUE.
+
+END SUBROUTINE
+
+END MODULE
