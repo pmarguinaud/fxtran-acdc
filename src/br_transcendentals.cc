e9cfc980dce62a45c5edc9e76ec5816c9632f42f
Nicolas Penigaud <nicolas.penigaud@meteo.fr>
2025-07-29 12:17:56 +0000

    add more precise version of erf

--- src/br_transcendentals.cc
+++ src/br_transcendentals.cc
@@ -22,8 +22,8 @@ namespace bitrep
  *************/
  
 static const double const_2_over_pi = 6.3661977236758138e-1;
-
-
+static const double halfLogTwoPi = 0.91893853320467274178032973640562;
+static const double squareRootOfPi = 1.77245385090551602729816748334;
 
 /*****************************************
  * FORWARD DECLARATION OF SOME FUNCTIONS *
@@ -35,6 +35,10 @@ double log1p(double);
 double log(double);
 double log_gamma(double);
 double gamma(double);
+double __cheb_eval(const double  cs[],int order,double x);
+double __erfseries(double x);
+double erfc(double x);
+
 
 /********************
  * HELPER FUNCTIONS *
@@ -890,33 +894,167 @@ double exp(double x)
 #pragma acc routine seq
 double erf(double x)
 {
-    //constants
-    double a1 =  0.254829592;
-    double a2 = -0.284496736;
-    double a3 =  1.421413741;
-    double a4 = -1.453152027;
-    double a5 =  1.061405429;
-    double p  =  0.3275911;
 
-    // Save the sign of x
-    int sign = 1;
-     if (x < 0)
-    sign = -1;
-    x = std::abs(x);
-    
-    // A&S formula 7.1.26
-    double t = 1.0/(1.0 + p*x);
-    double t2 ; 
-    t2 = __BITREPFMA(t,a5,a4);
-    t2 = __BITREPFMA(t2,t,a3);
-    t2 = __BITREPFMA(t2,t,a2);
-    t2 = __BITREPFMA(t2,t,a1);
-    t  = -t*exp(-x*x);
-    double y  = __BITREPFMA(t2,t,1.0);
-    //double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
-    
-    return sign*y;
+  if(std::abs(x) < 1.0) {
+    return __erfseries(x);
+  }
+  else {
+    return 1.0 - erfc(x);
+  }
+
 }
+
+#pragma acc routine seq
+double __cheb_eval(const double cs[],int order,double x)
+{
+
+
+  int j;
+  double d  = 0.0;
+  double dd = 0.0;
+
+  double y  = x ;
+  double y2 = 2.0 * y;
+
+
+  for(j = order; j>=1; j--) {
+    double temp = d;
+    d = __BITREPFMA(y2,d,-dd) + cs[j];
+    dd = temp;
+  }
+
+  d= __BITREPFMA(y,d,-dd);
+  d= __BITREPFMA(0.5,cs[0],d); 
+//  d = y*d - dd + 0.5 * cs[0];
+  
+  return d;
+}
+
+#pragma acc routine seq
+double erfc(double x)
+{
+
+const double erfc_xlt1_cs[20] = {
+  1.06073416421769980345174155056,
+ -0.42582445804381043569204735291,
+  0.04955262679620434040357683080,
+  0.00449293488768382749558001242,
+ -0.00129194104658496953494224761,
+ -0.00001836389292149396270416979,
+  0.00002211114704099526291538556,
+ -5.23337485234257134673693179020e-7,
+ -2.78184788833537885382530989578e-7,
+  1.41158092748813114560316684249e-8,
+  2.72571296330561699984539141865e-9,
+ -2.06343904872070629406401492476e-10,
+ -2.14273991996785367924201401812e-11,
+  2.22990255539358204580285098119e-12,
+  1.36250074650698280575807934155e-13,
+ -1.95144010922293091898995913038e-14,
+ -6.85627169231704599442806370690e-16,
+  1.44506492869699938239521607493e-16,
+  2.45935306460536488037576200030e-18,
+ -9.29599561220523396007359328540e-19
+};
+
+const double erfc_x15_cs[25] = {
+  0.44045832024338111077637466616,
+ -0.143958836762168335790826895326,
+  0.044786499817939267247056666937,
+ -0.013343124200271211203618353102,
+  0.003824682739750469767692372556,
+ -0.001058699227195126547306482530,
+  0.000283859419210073742736310108,
+ -0.000073906170662206760483959432,
+  0.000018725312521489179015872934,
+ -4.62530981164919445131297264430e-6,
+  1.11558657244432857487884006422e-6,
+ -2.63098662650834130067808832725e-7,
+  6.07462122724551777372119408710e-8,
+ -1.37460865539865444777251011793e-8,
+  3.05157051905475145520096717210e-9,
+ -6.65174789720310713757307724790e-10,
+  1.42483346273207784489792999706e-10,
+ -3.00141127395323902092018744545e-11,
+  6.22171792645348091472914001250e-12,
+ -1.26994639225668496876152836555e-12,
+  2.55385883033257575402681845385e-13,
+ -5.06258237507038698392265499770e-14,
+  9.89705409478327321641264227110e-15,
+ -1.90685978789192181051961024995e-15,
+  3.50826648032737849245113757340e-16
+};
+
+const double erfc_x510_cs[20] = {
+  1.11684990123545698684297865808,
+  0.003736240359381998520654927536,
+ -0.000916623948045470238763619870,
+  0.000199094325044940833965078819,
+ -0.000040276384918650072591781859,
+  7.76515264697061049477127605790e-6,
+ -1.44464794206689070402099225301e-6,
+  2.61311930343463958393485241947e-7,
+ -4.61833026634844152345304095560e-8,
+  8.00253111512943601598732144340e-9,
+ -1.36291114862793031395712122089e-9,
+  2.28570483090160869607683087722e-10,
+ -3.78022521563251805044056974560e-11,
+  6.17253683874528285729910462130e-12,
+ -9.96019290955316888445830597430e-13,
+  1.58953143706980770269506726000e-13,
+ -2.51045971047162509999527428316e-14,
+  3.92607828989125810013581287560e-15,
+ -6.07970619384160374392535453420e-16,
+  9.12600607264794717315507477670e-17
+};
+
+
+  double ax = std::abs(x);
+  double e_val;
+
+  /* CHECK_POINTER(result) */
+
+  if(ax <= 1.0) {
+    double t = __BITREPFMA(2.0,ax,-1.0);
+    e_val= __cheb_eval(erfc_xlt1_cs,19 ,t);
+  }
+  else if(ax <= 5.0) {
+    double ex2 = exp(-x*x);
+    double t = 0.5*(ax-3.0);
+    e_val= ex2* __cheb_eval(erfc_x15_cs,24 ,t);
+  }
+ else {
+   double exterm = exp(-x*x) / ax;
+   double t = (__BITREPFMA(2.0,ax,-15.0))/5.0;
+   e_val= exterm*(__cheb_eval(erfc_x510_cs,19 ,t));
+ }
+
+  if(x < 0.0) {
+    return  2.0 - e_val;
+  }
+  else {
+    return  e_val;
+  }
+
+}
+
+
+#pragma acc routine seq
+double __erfseries(double x)
+{
+  double coef = x;
+  double e    = coef;
+  double del;
+  int k;
+  for (k=1; k<30; ++k) {
+    coef *= -x*x/k;
+    del   = coef/(__BITREPFMA(2.0,k,1.0));
+    e += del;
+  }
+  return 2.0 / squareRootOfPi  * e;
+
+}
+
 
 #pragma acc routine seq
 double gamma
@@ -1086,7 +1224,6 @@ double log_gamma
     }
     double series = sum/x;
 
-    static const double halfLogTwoPi = 0.91893853320467274178032973640562;
     double loggamma ;
         loggamma = (x - 0.5)*log(x) ;
         loggamma = loggamma - x ;
@@ -1131,6 +1268,8 @@ double br_log  (double x) { return bitrep::log  (x); }
 double br_log1p(double x) { return bitrep::log1p(x); }
 #pragma acc routine seq
 double br_exp  (double x) { return bitrep::exp  (x); }
+#pragma acc routine seq
+double br_erfc  (double x) { return bitrep::erfc  (x); }
 #pragma acc routine seq
 double br_erf  (double x) { return bitrep::erf  (x); }
 #pragma acc routine seq
