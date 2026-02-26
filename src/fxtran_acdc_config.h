dae6fef3662c744c5d24d6e5e78e3945a7e308a2
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-02-26 13:30:43 +0000

    Compile & run NVIDIA with OpenMPTarget

--- src/fxtran_acdc_config.h
+++ src/fxtran_acdc_config.h
@@ -8,6 +8,8 @@
 
 #if FXTRAN_ACDC_PRAGMA == FXTRAN_ACDC_OPENACC
 #define _FXTRAN_USE_OPENACC
+#elif FXTRAN_ACDC_PRAGMA == FXTRAN_ACDC_OPENMPTARGET
+#define _FXTRAN_USE_OPENMPTARGET
 #else
 #error No accelerator pragma defined
 #endif
@@ -19,6 +21,7 @@
 #endif
 
 #ifdef _FXTRAN_USE_OPENACC
+
 #define FXTRAN_ACDC_ROUTINE_SEQ          !$ACC ROUTINE SEQ
 #define FXTRAN_ACDC_ENTER_DATA_CREATE(x) !$ACC ENTER DATA CREATE (x)
 #define FXTRAN_ACDC_EXIT_DATA_DELETE(x)  !$ACC EXIT DATA DELETE (x)
@@ -28,6 +31,20 @@
 
 #define fxtran_acdc_routine_seq_begin    _Pragma ("acc routine seq") 
 #define fxtran_acdc_routine_seq_end      //
+
+#endif
+
+#ifdef _FXTRAN_USE_OPENMPTARGET
+
+#define FXTRAN_ACDC_ROUTINE_SEQ          !$OMP DECLARE TARGET
+#define FXTRAN_ACDC_ENTER_DATA_CREATE(x) !$OMP TARGET ENTER DATA MAP (ALLOC: x)
+#define FXTRAN_ACDC_EXIT_DATA_DELETE(x)  !$OMP TARGET EXIT DATA MAP (DELETE: x)
+#define FXTRAN_ACDC_UPDATE_DEVICE(x)     !$OMP TARGET UPDATE TO (x)
+#define FXTRAN_ACDC_ENTER_DATA_ATTACH(x) !$OMP TARGET ENTER DATA MAP (TO: x)
+#define FXTRAN_ACDC_EXIT_DATA_DETACH(x)  !$OMP TARGET EXIT DATA MAP (RELEASE: x)
+
+#define fxtran_acdc_routine_seq_begin    _Pragma ("omp declare target") 
+#define fxtran_acdc_routine_seq_end      _Pragma ("omp end declare target")
 
 #endif
 
