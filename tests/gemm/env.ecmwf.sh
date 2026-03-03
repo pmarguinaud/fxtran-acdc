7dc87354971e0012c87f96b7fc977cd13aa3bfc0
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-03-03 10:39:45 +0000

    Compile on CPU (nvidia@ecmwf)

--- tests/gemm/env.ecmwf.sh
+++ tests/gemm/env.ecmwf.sh
@@ -6,7 +6,7 @@ export MODULEPATH=/perm/sor/install/nvidia/hpc_sdk/modulefiles
 module load nvhpc/26.1
 
 export CUDA=12.9
-export NVTXLIBS="-lnvtx3interop"
+export NVTXLIBS="-L$NVHPC_ROOT/cuda/$CUDA/targets/x86_64-linux/lib -Wl,-rpath,$NVHPC_ROOT/cuda/$CUDA/targets/x86_64-linux/lib -lnvtx3interop"
 
 ARCHLIST="NVIDIA_OPENACC NVIDIA_OPENMPTARGET"
 
