b4b25370af67a3293712fa3543ca32f34c711d2d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-03-03 10:30:03 +0000

    Compile on CPU (NVIDIA@meteo)

--- tests/gemm/env.meteo.sh
+++ tests/gemm/env.meteo.sh
@@ -2,7 +2,7 @@
 module load gcc/nvhpc24.7
 
 export CUDA=12.5
-export NVTXLIBS="-lnvhpcwrapnvtx -lnvToolsExt"
+export NVTXLIBS="-L$NVHPC_ROOT/cuda/$CUDA/lib64 -Wl,-rpath,$NVHPC_ROOT/cuda/$CUDA/lib64 -lnvhpcwrapnvtx -lnvToolsExt"
 
 ARCHLIST="NVIDIA_OPENACC NVIDIA_OPENMPTARGET"
 
